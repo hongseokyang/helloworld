e87eaa51fdd67ad12454149eb36f49ad55c60062
bit <bit@DESKTOP-S8HQR48>
2019-08-30 13:35:01 +0900

    commit

--- helloworld/src/helloworld/HelloWorld2.java
+++ helloworld/src/helloworld/HelloWorld2.java
@@ -1,9 +1,9 @@
 package helloworld;
 
-public class HelloWorld {
+public class HelloWorld2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("HelloWorld2");
 	}
 
 }
